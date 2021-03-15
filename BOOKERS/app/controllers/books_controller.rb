847396a9e44a5d61d1efc34796d6113c0de5715e
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-03-15 13:18:20 +0000

    [Finish]

--- BOOKERS/app/controllers/books_controller.rb
+++ BOOKERS/app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
 
   # rails4章で追加
-  def new
+  def index
     # rails５章で追加 eachメソッド
     @books = Book.all
 
@@ -13,13 +13,14 @@ class BooksController < ApplicationController
   # rails4章で追加
   def create
     # １. データを新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
+    @books = Book.all
     # ２. データをデータベースに保存するためのsaveメソッド実行
-    if book.save
+    if @book.save
       # ３. トップ画面へリダイレクト
       # redirect_to '/top'
       # ５章で追加 詳細画面へリダイレクト
-      redirect_to book_path(book.id), notice:'Book was successfully created.'
+      redirect_to book_path(@book.id), notice:'Book was successfully created.'
       # redirect_to '/books/new/:id'
     else
       render "new"
@@ -37,16 +38,20 @@ class BooksController < ApplicationController
 
   # rails6章editのdb更新
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice:'Book was successfully updated.'
+    @books = Book.all
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        redirect_to book_path(@book.id), notice:'Book was successfully updated.'
+      else
+        render "edit"
+      end
   end
 
   # rails 8章削除機能追加
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books/new'
+    redirect_to '/books', notice:'Book was successfully destroyed.'
   end
 
 #これが１番下に来るようにする
--- BOOKERS/config/routes.rb
+++ BOOKERS/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # rails 3章
-  get 'books/new'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -10,22 +10,24 @@ Rails.application.routes.draw do
   # rails ルートパス 設定
   root to: 'homes#top'
 
-  # rails 4章
-  post 'books' => 'books#create'
 
-  #rails 5章
-  get 'books/new' => 'books#index'
+resources :books
+  # # rails 4章
+  # post 'books' => 'books#create'
 
-  # rails5章 id
-  # .../todolists/1 や .../todolists/3 に該当する
-  get 'books/new/:id' => 'books#show', as: 'book'
+  # # #rails 5章
+  # get 'books' => 'books#index'
 
-  # rails6章 編集機能
-  get 'books/new/:id/edit' => 'books#edit', as: 'edit_book'
+  # # rails5章 id
+  # # .../todolists/1 や .../todolists/3 に該当する
+  # get 'books/new/:id' => 'books#show', as: 'book'
 
-  # rails6章 編集機能db更新
-  patch 'books/new/:id' => 'books#update', as: 'update_book'
+  # # rails6章 編集機能
+  # get 'books/new/:id/edit' => 'books#edit', as: 'edit_book'
 
-  # rails8章 削除機能の追加
-  delete 'books/new/:id' => 'books#destroy', as: 'destroy_book'
+  # # rails6章 編集機能db更新
+  # patch 'books/new/:id' => 'books#update', as: 'update_book'
+
+  # # rails8章 削除機能の追加
+  # delete 'books/new/:id' => 'books#destroy', as: 'destroy_book'
 end
